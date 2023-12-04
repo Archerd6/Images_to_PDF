7b11913a2c8e39d763bae96d913c6c7e3d6bedcc
Archerd6 <archerd@uma.es>
2023-12-04 14:38:49 +0100

    Updated Installation

---
+++ Version imagen 1-1/Aa Instalacion/Instalation.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt install python3-pip
+
+sudo pip3 install ocrmypdf
+
+sudo apt-get install tesseract-ocr-spa
+
+sudo apt install poppler-utils
+
+sudo apt install imagemagick
+
+sudo apt install libimage-exiftool-perl
+
+sudo apt install exiftran
